7e5e8c63bd4c0c15d7232eeb25fee48adce13b52
salauddinmd47 <salauddinbd47@gmail.com>
2022-11-09 22:46:26 +0600

    differnt state manage with react state lifting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ 
+import { useState } from "react";
+import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+
+const initailState = [
+  {
+    id:1,
+    count:0
+  },
+  {
+    id:2,
+    count:0
+  }
+]
 
 function App() {
+  const [state, setState] = useState(initailState);
+  
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+};
+   
+  const increment = (id)=>{
+    const updatedCount = state.map(c=> {
+      if(c.id === id){
+        return {
+          ...c,
+          count:c.count +1
+        }
+      }else{
+        return {...c}
+      }
+    } 
+    )
+    setState(updatedCount)
+  }
+  const decrement = (id)=>{
+
+    const updatedCount = state.map(c=> {
+      if(c.id === id){
+        return {
+          ...c,
+          count:c.count -1
+        }
+      }else{
+        return {...c}
+      }
+    } 
+    )
+    setState(updatedCount)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+      <div class="max-w-md mx-auto mt-10 space-y-5">
+       {
+        state.map(counter=>  <Counter key={counter.id} count={counter.count} id={counter.id} increment={increment} decrement={decrement} />)
+       }
+         
+        <Stats count={totalCount()} />
+      </div>
     </div>
   );
 }
